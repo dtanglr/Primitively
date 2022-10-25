b460fd101858dd512b9f483e154868c434f97cc3
Stuart Blackburn <stuart@dtangl.com>
2022-10-25 20:46:48 +0100

    task: updated example sln

--- examples/Postcode.cs
+++ examples/Postcode.cs
@@ -2,5 +2,5 @@
 
 namespace Examples;
 
-[String(4, 8, Example = "M23 0LY", Pattern = "^[A-Z]{1,2}[0-9][A-Z0-9]? ?[0-9][A-Z]{2}$")]
+[String(4, 8, Example = "N20 1LP", Pattern = "^[A-Z]{1,2}[0-9][A-Z0-9]? ?[0-9][A-Z]{2}$")]
 public partial record struct Postcode;
