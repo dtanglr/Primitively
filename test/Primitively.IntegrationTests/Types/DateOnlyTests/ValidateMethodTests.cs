8769226cb3fb466642db29e1e2a077744d8909a2
Stuart Blackburn <stuart@dtangl.com>
2022-10-28 21:58:00 +0100

    task: add tests for the Validation method

---
+++ test/Primitively.IntegrationTests/Types/DateOnlyTests/ValidateMethodTests.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using Xunit;
+
+namespace Primitively.IntegrationTests.Types.DateOnlyTests;
+
+public class ValidateMethodTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("    ")]
+    [InlineData("0001-01-01")]
+    [InlineData("9999-99-99")]
+    [InlineData("2022-02-31")]
+    [InlineData("2022-31-01")]
+    [InlineData("2022/01/01")]
+    [InlineData("01/01/2022")]
+    [InlineData("31/01/2022")]
+    [InlineData("01/31/2022")]
+    [InlineData("2022-01-01", true)]
+    public void ConvertFromThisToThatWithExpectedResults(string value, bool isValid = false)
+    {
+        // Arrange
+        var validationContext = new ValidationContext(this);
+        var sut = BirthDate.Parse(value);
+
+        // Act
+        var result = sut.Validate(validationContext);
+
+        // Assert
+        if (isValid)
+        {
+            result.Should().BeEmpty();
+        }
+        else
+        {
+            result.Should().NotBeNull();
+            result.Should().HaveCount(1);
+        }
+    }
+}
---
+++ test/Primitively.IntegrationTests/Types/StringTests/ValidateMethodTests.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using Xunit;
+
+namespace Primitively.IntegrationTests.Types.StringTests;
+
+public class ValidateMethodTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("    ")]
+    [InlineData("G7123456")]
+    [InlineData("123456")]
+    [InlineData("1234567", true)]
+    [InlineData("0123456", true)]
+    public void ConvertFromThisToThatWithExpectedResults(string value, bool isValid = false)
+    {
+        // Arrange
+        var validationContext = new ValidationContext(this);
+        var sut = SevenDigits.Parse(value);
+
+        // Act
+        var result = sut.Validate(validationContext);
+
+        // Assert
+        if (isValid)
+        {
+            result.Should().BeEmpty();
+        }
+        else
+        {
+            result.Should().NotBeNull();
+            result.Should().HaveCount(1);
+        }
+    }
+}
