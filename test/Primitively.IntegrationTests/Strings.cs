51b925ac1e195fa91bec9ef81dea868788ad00dc
Stuart Blackburn <stuart@dtangl.com>
2022-11-01 00:53:03 +0000

    task: removed unused/untests string primitives

--- test/Primitively.IntegrationTests/Strings.cs
+++ test/Primitively.IntegrationTests/Strings.cs
@@ -7,12 +7,3 @@ public partial record struct SevenDigits;
 
 [String(8, Pattern = "^[0-9]{8}$", Example = "12345678")]
 public partial record struct EightDigits;
-
-[String(9, Pattern = "^[0-9]{9}$", Example = "123456789")]
-public partial record struct NineDigits;
-
-[String(10, Pattern = "^[0-9]{10}$", Example = "1234567890")]
-public partial record struct TenDigits;
-
-[String(11, Pattern = "^[0-9]{11}$", Example = "12345678901")]
-public partial record struct ElevenDigits;
