46fbf8907188dadd70c25319cc69c0bc70471ed1
Stuart Blackburn <stuart@dtangl.com>
2022-11-11 09:43:06 +0000

    task: change the Primitively assembly name to Primitively.SourceGeneration

--- src/Primitively/Structs.cs
+++ src/Primitively/Structs.cs
@@ -11,7 +11,7 @@ namespace Primitively;
 
 /// <inheritdoc />
 [Generator]
-public class SourceGeneration : IIncrementalGenerator
+public class Structs : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
