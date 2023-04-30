9d2b8372cea4953e70e745d018dc11ded59c3d6e
Stuart Blackburn <stuart@dtangl.com>
2023-04-30 03:38:07 +0100

    task: added initial code to handle MongoDb Serialization of Primitively types

---
+++ src/Primitively.MongoDb/PrimitivelyBsonSerializer.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using static Primitively.MongoDb.PrimitivelyBson;
+
+namespace Primitively.MongoDb;
+
+public class PrimitivelyBsonSerializer<TPrimitive> : SerializerBase<TPrimitive>
+    where TPrimitive : struct, IPrimitive
+{
+    public override TPrimitive Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        if (context.Reader.CurrentBsonType == BsonType.Null)
+        {
+            context.Reader.ReadNull();
+
+            return new();
+        }
+
+        return Deserialize<TPrimitive>(context);
+    }
+
+    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive value)
+    {
+        Serialize<TPrimitive>(context, value);
+    }
+}
---
+++ src/Primitively.MongoDb/PrimitivelyNullableBsonSerializer.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using static Primitively.MongoDb.PrimitivelyBson;
+
+namespace Primitively.MongoDb;
+
+public class PrimitivelyNullableBsonSerializer<TPrimitive> : SerializerBase<TPrimitive?>
+    where TPrimitive : struct, IPrimitive
+{
+    public override TPrimitive? Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        if (context.Reader.CurrentBsonType == BsonType.Null)
+        {
+            context.Reader.ReadNull();
+
+            return null;
+        }
+
+        return Deserialize<TPrimitive>(context);
+    }
+
+    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive? value)
+    {
+        if (value == null)
+        {
+            context.Writer.WriteNull();
+
+            return;
+        }
+
+        Serialize<TPrimitive>(context, (TPrimitive)value);
+    }
+}
