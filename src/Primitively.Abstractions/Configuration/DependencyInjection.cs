867bb888087b57e9ab40665d1ddeeb3c7702af49
Stuart Blackburn <stuart@dtangl.com>
2023-10-25 01:06:01 +0100

    task: removed DI AddPrimitively overload method

--- src/Primitively.Abstractions/Configuration/DependencyInjection.cs
+++ src/Primitively.Abstractions/Configuration/DependencyInjection.cs
@@ -4,25 +4,10 @@ namespace Primitively.Configuration;
 
 public static class DependencyInjection
 {
-    public static PrimitivelyConfigurator AddPrimitively(this IServiceCollection services, params IPrimitiveRepository[] repositories)
+    public static PrimitivelyConfigurator AddPrimitively(this IServiceCollection services, Action<PrimitivelyOptions>? optionsAction = null)
     {
         var options = new PrimitivelyOptions();
-
-        if (repositories != null)
-        {
-            foreach (var repository in repositories)
-            {
-                options.Registry.Add(repository);
-            }
-        }
-
-        return new PrimitivelyConfigurator(services, options);
-    }
-
-    public static PrimitivelyConfigurator AddPrimitively(this IServiceCollection services, Action<PrimitivelyOptions> optionsAction)
-    {
-        var options = new PrimitivelyOptions();
-        optionsAction.Invoke(options);
+        optionsAction?.Invoke(options);
 
         return new PrimitivelyConfigurator(services, options);
     }
