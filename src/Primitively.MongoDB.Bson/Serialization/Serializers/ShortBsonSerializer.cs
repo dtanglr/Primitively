eef9ed89d90ee9725e72b2a2aa6ced4dcc7f773b
Stuart Blackburn <stuart@dtangl.com>
2023-10-30 20:15:46 +0000

    task: updated ShortBsonSerializer to use the MongoDB Int16Serializer

--- src/Primitively.MongoDB.Bson/Serialization/Serializers/ShortBsonSerializer.cs
+++ src/Primitively.MongoDB.Bson/Serialization/Serializers/ShortBsonSerializer.cs
@@ -1,29 +1,137 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
 using MongoDB.Bson.Serialization.Serializers;
 
 namespace Primitively.MongoDB.Bson.Serialization.Serializers;
 
-public class ShortBsonSerializer<TPrimitive> : SerializerBase<TPrimitive>
+/// <summary>
+/// Represents a serializer for Primitively types that encapsulate a Short value.
+/// </summary>
+public class ShortBsonSerializer<TPrimitive> : StructSerializerBase<TPrimitive>, IRepresentationConfigurable<ShortBsonSerializer<TPrimitive>>, IRepresentationConverterConfigurable<ShortBsonSerializer<TPrimitive>>
     where TPrimitive : struct, IShort
 {
-    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive value)
+    private readonly Int16Serializer _serializer;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="serializer">The serializer.</param>
+    private ShortBsonSerializer(Int16Serializer serializer)
     {
-        context.Writer.WriteInt32(value.Value);
+        _serializer = serializer;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    public ShortBsonSerializer()
+    {
+        _serializer = new Int16Serializer();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="representation">The representation.</param>
+    public ShortBsonSerializer(BsonType representation)
+    {
+        _serializer = new Int16Serializer(representation);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="representation">The representation.</param>
+    /// <param name="converter">The converter.</param>
+    public ShortBsonSerializer(BsonType representation, RepresentationConverter converter)
+    {
+        _serializer = new Int16Serializer(representation, converter);
+    }
+
+    /// <summary>
+    /// Gets a cached instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    public static ShortBsonSerializer<TPrimitive> Instance { get; } = new();
+
+    /// <summary>
+    /// Gets the representation.
+    /// </summary>
+    public BsonType Representation => _serializer.Representation;
+
+    /// <summary>
+    /// Gets the converter.
+    /// </summary>
+    public RepresentationConverter Converter => _serializer.Converter;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShortBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="serializer">The serializer.</param>
+    public static ShortBsonSerializer<TPrimitive> Create(Int16Serializer serializer) => new(serializer);
+
+    /// <summary>
+    /// Deserializes a value.
+    /// </summary>
+    /// <param name="context">The deserialization context.</param>
+    /// <param name="args">The deserialization args.</param>
+    /// <returns>A deserialized value.</returns>
     public override TPrimitive Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
-        if (context.Reader.CurrentBsonType == BsonType.Null)
-        {
-            context.Reader.ReadNull();
-
-            // Return default if null
-            return new();
-        }
-
-        var value = Convert.ToInt16(context.Reader.ReadInt32());
+        var value = _serializer.Deserialize(context, args);
 
         return (TPrimitive)Activator.CreateInstance(typeof(TPrimitive), value)!;
+    }
+
+    /// <summary>
+    /// Serializes a value.
+    /// </summary>
+    /// <param name="context">The serialization context.</param>
+    /// <param name="args">The serialization args.</param>
+    /// <param name="value">The object.</param>
+    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive value)
+    {
+        _serializer.Serialize(context, args, value.Value);
+    }
+
+    /// <summary>
+    /// Returns a serializer that has been reconfigured with the specified item serializer.
+    /// </summary>
+    /// <param name="converter">The converter.</param>
+    /// <returns>The reconfigured serializer.</returns>
+    public ShortBsonSerializer<TPrimitive> WithConverter(RepresentationConverter converter)
+    {
+        if (converter == _serializer.Converter)
+        {
+            return this;
+        }
+
+        return new ShortBsonSerializer<TPrimitive>(_serializer.Representation, converter);
+    }
+
+    /// <summary>
+    /// Returns a serializer that has been reconfigured with the specified representation.
+    /// </summary>
+    /// <param name="representation">The representation.</param>
+    /// <returns>The reconfigured serializer.</returns>
+    public ShortBsonSerializer<TPrimitive> WithRepresentation(BsonType representation)
+    {
+        if (representation == _serializer.Representation)
+        {
+            return this;
+        }
+
+        return new ShortBsonSerializer<TPrimitive>(representation, _serializer.Converter);
+    }
+
+    // explicit Shorterface implementations
+    IBsonSerializer IRepresentationConverterConfigurable.WithConverter(RepresentationConverter converter)
+    {
+        return WithConverter(converter);
+    }
+
+    IBsonSerializer IRepresentationConfigurable.WithRepresentation(BsonType representation)
+    {
+        return WithRepresentation(representation);
     }
 }
