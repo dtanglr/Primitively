6f47684ab426d436aa26ead7aa88669294bf345b
Stuart Blackburn <stuart@dtangl.com>
2023-10-30 20:37:40 +0000

    task: updated ByteBsonSerializer to use the MongoDB ByteSerializer

--- src/Primitively.MongoDB.Bson/Serialization/Serializers/ByteBsonSerializer.cs
+++ src/Primitively.MongoDB.Bson/Serialization/Serializers/ByteBsonSerializer.cs
@@ -4,26 +4,98 @@ using MongoDB.Bson.Serialization.Serializers;
 
 namespace Primitively.MongoDB.Bson.Serialization.Serializers;
 
-public class ByteBsonSerializer<TPrimitive> : SerializerBase<TPrimitive>
+/// <summary>
+/// Represents a serializer for Primitively types that encapsulate a Byte value.
+/// </summary>
+public class ByteBsonSerializer<TPrimitive> : StructSerializerBase<TPrimitive>, IRepresentationConfigurable<ByteBsonSerializer<TPrimitive>>
     where TPrimitive : struct, IByte
 {
-    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive value)
+    private readonly ByteSerializer _serializer;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ByteBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="serializer">The serializer.</param>
+    private ByteBsonSerializer(ByteSerializer serializer)
     {
-        context.Writer.WriteInt32(value.Value);
+        _serializer = serializer;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ByteBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    public ByteBsonSerializer()
+    {
+        _serializer = new ByteSerializer();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ByteBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="representation">The representation.</param>
+    public ByteBsonSerializer(BsonType representation)
+    {
+        _serializer = new ByteSerializer(representation);
+    }
+
+    /// <summary>
+    /// Gets a cached instance of the <see cref="ByteBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    public static ByteBsonSerializer<TPrimitive> Instance { get; } = new();
+
+    /// <summary>
+    /// Gets the representation.
+    /// </summary>
+    public BsonType Representation => _serializer.Representation;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ByteBsonSerializer{TPrimitive}"/> class.
+    /// </summary>
+    /// <param name="serializer">The serializer.</param>
+    public static ByteBsonSerializer<TPrimitive> Create(ByteSerializer serializer) => new(serializer);
+
+    /// <summary>
+    /// Deserializes a value.
+    /// </summary>
+    /// <param name="context">The deserialization context.</param>
+    /// <param name="args">The deserialization args.</param>
+    /// <returns>A deserialized value.</returns>
     public override TPrimitive Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
-        if (context.Reader.CurrentBsonType == BsonType.Null)
-        {
-            context.Reader.ReadNull();
-
-            // Return default if null
-            return new();
-        }
-
-        var value = Convert.ToByte(context.Reader.ReadInt32());
+        var value = _serializer.Deserialize(context, args);
 
         return (TPrimitive)Activator.CreateInstance(typeof(TPrimitive), value)!;
+    }
+
+    /// <summary>
+    /// Serializes a value.
+    /// </summary>
+    /// <param name="context">The serialization context.</param>
+    /// <param name="args">The serialization args.</param>
+    /// <param name="value">The object.</param>
+    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TPrimitive value)
+    {
+        _serializer.Serialize(context, args, value.Value);
+    }
+
+    /// <summary>
+    /// Returns a serializer that has been reconfigured with the specified representation.
+    /// </summary>
+    /// <param name="representation">The representation.</param>
+    /// <returns>The reconfigured serializer.</returns>
+    public ByteBsonSerializer<TPrimitive> WithRepresentation(BsonType representation)
+    {
+        if (representation == _serializer.Representation)
+        {
+            return this;
+        }
+
+        return new ByteBsonSerializer<TPrimitive>(representation);
+    }
+
+    // explicit Byteerface implementations
+    IBsonSerializer IRepresentationConfigurable.WithRepresentation(BsonType representation)
+    {
+        return WithRepresentation(representation);
     }
 }
