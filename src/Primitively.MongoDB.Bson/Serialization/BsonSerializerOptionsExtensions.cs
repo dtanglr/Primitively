8b5679eac2b5f889e4ae968e25608d2f261ad6e8
Stuart Blackburn <stuart@dtangl.com>
2024-01-09 22:13:53 +0000

    task: moved file

---
+++ src/Primitively.MongoDB.Bson/Serialization/BsonSerializerOptionsExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Primitively.MongoDB.Bson.Serialization;
+
+public static class BsonSerializerOptionsExtensions
+{
+    public static Type GetSerializerType(this IBsonSerializerOptions _, Type primitiveType, Type serializerType)
+    {
+        // Construct a Bson serializer for the given Primitively type using the options
+        return serializerType.IsGenericTypeDefinition ? serializerType.MakeGenericType(primitiveType) : serializerType;
+    }
+}
