cce2ed25efbb0090e48ca651b965ad42800a0f28
Stuart Blackburn <stuart@dtangl.com>
2024-07-26 22:36:03 +0100

    task: added missing xml comments

--- src/Primitively.Abstractions/INumericInfo.cs
+++ src/Primitively.Abstractions/INumericInfo.cs
@@ -1,10 +1,32 @@
 ﻿namespace Primitively;
 
+/// <summary>
+/// Defines a contract for providing metadata about a numeric type.
+/// </summary>
 public interface INumericInfo
 {
+    /// <summary>
+    /// Gets a function that creates an instance of the numeric type from a string representation.
+    /// </summary>
     Func<string?, IPrimitive> CreateFrom { get; init; }
+
+    /// <summary>
+    /// Gets the data type of the numeric type.
+    /// </summary>
     DataType DataType { get; init; }
+
+    /// <summary>
+    /// Gets an example value of the numeric type in string format.
+    /// </summary>
     string? Example { get; init; }
+
+    /// <summary>
+    /// Gets the .NET type of the numeric type.
+    /// </summary>
     Type Type { get; init; }
+
+    /// <summary>
+    /// Gets the .NET value type that the numeric type encapsulates.
+    /// </summary>
     Type ValueType { get; init; }
 }
